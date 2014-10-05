57e5c05134a4ffd83cb7896571b8d992dc719f03
Gareth Davies <git@garethtdavies.com>
2014-10-05 15:21:02 -0700

    Adding test for multiple addresses

--- tests/ChainCoreTest.php
+++ tests/ChainCoreTest.php
@@ -36,4 +36,18 @@ class ChainCoreTest extends \Guzzle\Tests\GuzzleTestCase
         $chain = new Cbix\ChainCore($this->client);
         $chain->get_address('17x23dNjXJLzGMev6R63uyRhMWP1VHawKc');
     }
+
+    public function test_get_address_returns_correct_number_of_results_for_multiple_addresses()
+    {
+        $mock = new GuzzleHttp\Subscriber\Mock([
+            __DIR__ . '/mock/addresses.txt'
+        ]);
+
+        $this->client->getEmitter()->attach($mock);
+
+        $chain = new Cbix\ChainCore($this->client);
+        $result = $chain->get_address('17x23dNjXJLzGMev6R63uyRhMWP1VHawKc', '1EX1E9n3bPA1zGKDV5iHY2MnM7n5tDfnfH');
+
+        $this->assertCount(2, $result);
+    }
 }
