a1dd2a91223b8492541957ec8118ffc031ed552b
Gareth Davies <git@garethtdavies.com>
2014-10-05 15:54:13 -0700

    Testing address transactions

--- tests/ChainCoreTest.php
+++ tests/ChainCoreTest.php
@@ -50,4 +50,33 @@ class ChainCoreTest extends \Guzzle\Tests\GuzzleTestCase
 
         $this->assertCount(2, $result);
     }
+
+    public function test_get_address_transactions_returns_correct_response()
+    {
+        $mock = new GuzzleHttp\Subscriber\Mock([
+            __DIR__ . '/mock/address_transactions.txt'
+        ]);
+
+        $this->client->getEmitter()->attach($mock);
+
+        $chain = new Cbix\ChainCore($this->client);
+        $result = $chain->get_address_transactions('1K4nPxBMy6sv7jssTvDLJWk1ADHBZEoUVb', ['limit' => 2]);
+
+        $this->assertCount(2, $result);
+        $this->assertEquals('0bf0de38c26195919179f42d475beb7a6b15258c38b57236afdd60a07eddd2cc', $result[0]->hash);
+    }
+
+    public function test_get_address_transactions_throws_an_exception()
+    {
+        $this->setExpectedException('Cbix\ChainException');
+
+        $mock = new GuzzleHttp\Subscriber\Mock([
+            new GuzzleHttp\Message\Response(400),
+        ]);
+
+        $this->client->getEmitter()->attach($mock);
+
+        $chain = new Cbix\ChainCore($this->client);
+        $chain->get_address_transactions('1K4nPxBMy6sv7jssTvDLJWk1ADHBZEoUVb');
+    }
 }
